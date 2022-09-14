10988e8b5cb889d5381bbe40469220e208e5658a
Filipe Abreu <filabreu@gmail.com>
2022-09-13 21:10:13 -0300

    Recursively count length of a list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    list_length(list, 0)
+  end
+
+  def list_length([], acc) do
+    acc
+  end
+
+  def list_length([_ | tail], acc) do
+    list_length(tail, acc + 1)
   end
 end
